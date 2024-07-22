61209e131513392b12bf1830545ea6910677f02d
Cristian Paniagua <tenondecrpc@gmail.com>
2024-07-21 22:59:31 -0400

    feat(pipeline): Added role for pipeline

--- lib/main-stack.ts
+++ lib/main-stack.ts
@@ -9,14 +9,13 @@ export class MainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // cambiar a publico para la organización
     const repositoryArn = ssm.StringParameter.valueFromLookup(this, '/my/application/connection/repository/arn');
     const repositoryName = ssm.StringParameter.valueFromLookup(this, '/my/application/connection/repository/infra/web/name');
     const repositoryBranch = ssm.StringParameter.valueFromLookup(this, '/my/application/connection/repository/infra/web/branch');
     const accountIdDeploy = ssm.StringParameter.valueFromLookup(this, '/account/id/deploy');
     const regionDeploy = ssm.StringParameter.valueFromLookup(this, '/account/region/deploy');
 
-    const pipeline = new pipelines.CodePipeline(this, "MainPipeline", {
+    const pipeline = new pipelines.CodePipeline(this, "Web", {
       crossAccountKeys: true,
       synth: new pipelines.ShellStep("Synth", {
         input: pipelines.CodePipelineSource.connection(
@@ -37,6 +36,18 @@ export class MainStack extends cdk.Stack {
     );
 
     pipeline.buildPipeline();
+
+    // TODO: Improve role grant for get parameter store
+    const pipelineRole = pipeline.pipeline.role;
+    const parameterStorePolicy = new iam.PolicyStatement({
+      actions: [
+        'ssm:GetParameter',
+        'ssm:GetParameters',
+        'ssm:GetParametersByPath'
+      ],
+      resources: [`arn:aws:ssm:${props?.env?.region}:${props?.env?.account}:parameter/*`]
+    });
+    pipelineRole.addToPrincipalPolicy(parameterStorePolicy);
 
     pipeline.pipeline.artifactBucket.grantRead(new iam.AccountPrincipal(accountIdDeploy));
   }
